ce0bd5e2314dffac473d0bd57a6f9a4ed7d2d11b
Анна Кулибчук bc2024-5 <kulibchuk.a2006@gmail.com>
2024-11-14 15:18:29 +0200

    Added method get

---
+++ lab5.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const { Command } = require('commander');
+const fs = require('fs').promises;
+const http = require('http');
+const path = require('path');
+
+const program = new Command();
+const app = express();
+
+program
+  .requiredOption('-h, --host <host>', 'Server host')
+  .requiredOption('-p, --port <port>', 'Server port')
+  .requiredOption('-c, --cache <path>', 'Path to cache');
+
+program.parse(process.argv);
+
+const options = program.opts();
+const { host, port, cache } = options;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/UploadForm.html', async (req, res) => {
+  const filePath = path.join(__dirname, 'UploadForm.html'); 
+  try {
+    const htmlForm = await fs.readFile(filePath, 'utf-8');
+    res.send(htmlForm);
+  } catch (err) {
+    res.status(500).send('Error reading HTML form');
+  }
+});
+
+const server = http.createServer(app);
+server.listen(port, host, () => {
+  console.log(`Server is running at http://${host}:${port}`);
+});
